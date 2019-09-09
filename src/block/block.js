07ffcc0fffe210f9fcf98521f2b05c4f0cfac21d
Mark Marzeotti <mark@markmarzeotti.com>
2019-09-09 07:15:59 -0400

    built repeater field as minimal block

--- src/block/block.js
+++ src/block/block.js
@@ -9,11 +9,27 @@
 import './editor.scss';
 import './style.scss';
 
-const { __ } = wp.i18n; // Import __() from wp.i18n
-const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
+const {
+	__,
+} = wp.i18n;
+const {
+	registerBlockType,
+} = wp.blocks;
+const {
+	Button,
+	IconButton,
+	PanelBody,
+	TextControl,
+} = wp.components;
+const {
+	InspectorControls,
+} = wp.editor;
+const {
+	Fragment,
+} = wp.element;
 
 /**
- * Register: aa Gutenberg Block.
+ * Register: Repeater Gutenberg Block.
  *
  * Registers a new block provided a unique name and an object defining its
  * behavior. Once registered, the block is made editor as an option to any
@@ -26,73 +42,94 @@ const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.b
  *                             registered; otherwise `undefined`.
  */
 registerBlockType( 'cgb/block-gutenberg-repeater-field', {
-	// Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
-	title: __( 'gutenberg-repeater-field - CGB Block' ), // Block title.
-	icon: 'shield', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
-	category: 'common', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
+	title: __( 'gutenberg-repeater-field - CGB Block' ),
+	icon: 'shield',
+	category: 'common',
+	attributes: {
+		locations: {
+			type: 'array',
+			default: [],
+		},
+	},
 	keywords: [
 		__( 'gutenberg-repeater-field — CGB Block' ),
 		__( 'CGB Example' ),
 		__( 'create-guten-block' ),
 	],
-
-	/**
-	 * The edit function describes the structure of your block in the context of the editor.
-	 * This represents what the editor will render when the block is used.
-	 *
-	 * The "edit" property must be a valid function.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-	 *
-	 * @param {Object} props Props.
-	 * @returns {Mixed} JSX Component.
-	 */
 	edit: ( props ) => {
-		// Creates a <p class='wp-block-cgb-block-gutenberg-repeater-field'></p>.
-		return (
-			<div className={ props.className }>
-				<p>— Hello from the backend.</p>
-				<p>
-					CGB BLOCK: <code>gutenberg-repeater-field</code> is a new Gutenberg block
-				</p>
-				<p>
-					It was created via{ ' ' }
-					<code>
-						<a href="https://github.com/ahmadawais/create-guten-block">
-							create-guten-block
-						</a>
-					</code>.
-				</p>
-			</div>
-		);
-	},
+		const handleAddLocation = () => {
+			const locations = [ ...props.attributes.locations ];
+			locations.push( {
+				address: '',
+			} );
+			props.setAttributes( { locations } );
+		};
 
-	/**
-	 * The save function defines the way in which the different attributes should be combined
-	 * into the final markup, which is then serialized by Gutenberg into post_content.
-	 *
-	 * The "save" property must be specified and must be a valid function.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-	 *
-	 * @param {Object} props Props.
-	 * @returns {Mixed} JSX Frontend HTML.
-	 */
+		const handleRemoveLocation = ( index ) => {
+			const locations = [ ...props.attributes.locations ];
+			locations.splice( index, 1 );
+			props.setAttributes( { locations } );
+		};
+
+		const handleLocationChange = ( address, index ) => {
+			const locations = [ ...props.attributes.locations ];
+			locations[ index ].address = address;
+			props.setAttributes( { locations } );
+		};
+
+		let markerFields,
+			markerDisplay;
+
+		if ( props.attributes.locations.length ) {
+			markerFields = props.attributes.locations.map( ( location, index ) => {
+				return <Fragment key={ index }>
+					<TextControl
+						className="advanced-maps-block__marker-address"
+						placeholder="350 Fifth Avenue New York NY"
+						value={ props.attributes.locations[ index ].address }
+						onChange={ ( address ) => handleLocationChange( address, index ) }
+					/>
+					<IconButton
+						className="advanced-maps-block__remove-marker-address"
+						icon="no-alt"
+						label="Delete Marker"
+						onClick={ () => handleRemoveLocation( index ) }
+					/>
+				</Fragment>;
+			} );
+
+			markerDisplay = props.attributes.locations.map( ( location, index ) => {
+				return <p key={ index }>{ location.address }</p>;
+			} );
+		}
+
+		return [
+			<InspectorControls key="1">
+				<PanelBody title={ __( 'Locations' ) }>
+					{ markerFields }
+					<Button
+						isDefault
+						onClick={ handleAddLocation.bind( this ) }
+					>
+						{ __( 'Add Marker' ) }
+					</Button>
+				</PanelBody>
+			</InspectorControls>,
+			<div key="2" className={ props.className }>
+				<h2>Block</h2>
+				{ markerDisplay }
+			</div>,
+		];
+	},
 	save: ( props ) => {
+		const markerFields = props.attributes.locations.map( ( location, index ) => {
+			return <p key={ index }>{ location.address }</p>;
+		} );
+
 		return (
 			<div className={ props.className }>
-				<p>— Hello from the frontend.</p>
-				<p>
-					CGB BLOCK: <code>gutenberg-repeater-field</code> is a new Gutenberg block.
-				</p>
-				<p>
-					It was created via{ ' ' }
-					<code>
-						<a href="https://github.com/ahmadawais/create-guten-block">
-							create-guten-block
-						</a>
-					</code>.
-				</p>
+				<h2>Block</h2>
+				{ markerFields }
 			</div>
 		);
 	},
